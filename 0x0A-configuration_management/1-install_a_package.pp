4a9d23b0ba141a789bb9238ab0f5664ba9baa4da
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-01-03 17:15:56 +0100

    made changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # Install the puppet-lint package
-<<<<<<< HEAD
 package { 'puppet-lint':
   ensure   => '2.5.0',
-=======
-package { 'puppet-lint':
-  ensure   => '2.1.1',
->>>>>>> 7eb0301a8de6bdad7a83740d562d349aff6e0549
   provider => 'gem',
 }

370f2d8f4262f4c6c9849efcf87d36c4f0bc4025
Faitholo <omokhudufaithy@outlook.com>
2022-01-05 20:46:36 +0100

    Add files via upload

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Install Nginx web server (w/ Puppet)
 exec { 'server configuration':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.html; sudo sed -i "/server_name _;/a listen 80 location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
 }

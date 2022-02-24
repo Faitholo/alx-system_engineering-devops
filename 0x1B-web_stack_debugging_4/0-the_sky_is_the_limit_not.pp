01ad8535c56431889fdf6fe2b5bda9ffbc5bd964
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-02-24 21:13:28 +0100

    completed task 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fix our stack so that we get to 0 errors
+exec { 'file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
